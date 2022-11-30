0627b4e11d56062f690f38d3aba4f1e7e816e7b6
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-30 20:46:24 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
+
+    static boolean Palindrom(String str){
+      String reverse="";
+      for (int i=str.length()-1;i>=0;i--){
+          reverse+=str.charAt(i);
+
+
+      }
+        if (reverse.equals(str)){
+            return true;
+
+
+        }
+        else {
+            return false;
+        }
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Palindrom("kayak"));
+
     }
 }
